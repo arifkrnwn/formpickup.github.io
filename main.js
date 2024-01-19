16fa5fa90d54d952651d102c9d3c545cafa91615
arifkrnwn <arifkurniawan4577@gmail.com>
2024-01-19 18:20:23 +0700

    aiya

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ async function fetchDataFromAPI() {
       "https://script.google.com/macros/s/AKfycbyxmSpHry0izzjK4z1Va8vRsMXfz95om48-5HPoGhUVWOefcZZoldGKacs6wVGFY3yY/exec?action=getDropdown"
     );
     const jsonData = await response.json();
-    console.log(jsonData);
     return jsonData;
   } catch (error) {
     console.error("Error fetching data:", error);
@@ -21,8 +20,6 @@ async function fillDatalists() {
     const datalist2 = document.getElementById("driver");
     const data1 = jsonData.data1;
     const data2 = jsonData.data2;
-    console.log(data1);
-    console.log(data2);
 
     // Mengisi datalist pertama
     data1.forEach((item) => {
@@ -43,33 +40,40 @@ async function fillDatalists() {
 // Panggil fungsi untuk mengisi datalist saat halaman dimuat
 window.onload = fillDatalists;
 
-document.getElementById("myForm").addEventListener("submit", function (event) {
-  // Mencegah perilaku default formulir
-  event.preventDefault();
+document
+  .getElementById("formpickup")
+  .addEventListener("submit", async function (event) {
+    // Prevent default form submission
+    event.preventDefault();
 
-  // Mendapatkan data formulir
-  var formData = new FormData(this);
+    // Get form data
+    const formData = new FormData(this);
 
-  // Mengirim data ke API menggunakan fetch
-  fetch(
-    "https://script.google.com/macros/s/AKfycbyxmSpHry0izzjK4z1Va8vRsMXfz95om48-5HPoGhUVWOefcZZoldGKacs6wVGFY3yY/exec",
-    {
-      method: "POST",
-      body: formData,
-    }
-  )
-    .then((response) => {
+    try {
+      // Send data to the API using fetch
+      const response = await fetch(
+        "https://script.google.com/macros/s/AKfycbyxmSpHry0izzjK4z1Va8vRsMXfz95om48-5HPoGhUVWOefcZZoldGKacs6wVGFY3yY/exec",
+        {
+          method: "POST",
+          body: formData,
+        }
+      );
+
       if (!response.ok) {
-        throw new Error("Gagal mengirim data");
+        throw new Error("Gagal mengirim data"); // Throw error for non-200 status codes
       }
-      return response.json();
-    })
-    .then((data) => {
-      // Handle respons dari API (jika diperlukan)
-      console.log("Sukses mengirim data:", data);
-    })
-    .catch((error) => {
-      // Handle kesalahan
-      console.error("Error:", error);
-    });
-});
+
+      // Handle successful response
+      const responseData = await response.json();
+      console.log(responseData); // Log response data for debugging
+
+      // Example: Display a success message to the user
+      alert("Data berhasil dikirim!");
+    } catch (error) {
+      // Handle errors
+      console.error("Error:", error); // Log error for debugging
+
+      // Example: Display an error message to the user
+      alert("Terjadi kesalahan saat mengirim data");
+    }
+  });
